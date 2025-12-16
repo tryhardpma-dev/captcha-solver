8fd0f3906fbd84e8a8ef4ced0d33e0c45317af0b
tryhardpma-dev <vlatsynba@gmail.com>
2025-12-16 18:35:00 +0100

    new CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from backend.routes.captcha import router as captcha_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(captcha_router, prefix="/captcha")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
